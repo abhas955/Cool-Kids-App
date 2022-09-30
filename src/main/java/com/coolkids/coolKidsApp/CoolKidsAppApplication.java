cb11a4eda3fb2350799fc544b73bcdef1d0ee3a6
Abdullahi <abhas955@gmail.com>
2022-09-30 17:15:55 -0400

    project set-up

---
+++ src/main/java/com/coolkids/coolKidsApp/CoolKidsAppApplication.java
@@ -0,0 +1,13 @@
+package com.coolkids.coolKidsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolKidsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolKidsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolkids/coolKidsApp/CoolKidsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolkids.coolKidsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolKidsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
