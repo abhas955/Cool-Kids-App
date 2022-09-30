02907bf09975f3e1decf80336c973b7337a51818
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-09-30 19:33:37 -0400

    Created User Database
    Created the first user database. Two sample users are saved to the db and a query displays all users.

--- src/main/java/com/coolkids/coolKidsApp/CoolKidsAppApplication.java
+++ src/main/java/com/coolkids/coolKidsApp/CoolKidsAppApplication.java
@@ -1,13 +1,42 @@
 package com.coolkids.coolKidsApp;
 
+import com.coolkids.coolKidsApp.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class CoolKidsAppApplication {
+public class CoolKidsAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository repository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoolKidsAppApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.deleteAll();
+
+		//Save a couple of users
+		repository.save(new User("Jim", "Smith"));
+		repository.save(new User("Bob", "West"));
+
+		//fetch all users
+		System.out.println("Users found with findAll():");
+		for (User user : repository.findAll()) {
+			System.out.println(user);
+		}
+		System.out.println();
+
+		//fetch an individual user
+		// fetch an individual customer
+		System.out.println("User found with findByFirstName('Jim'):");
+		System.out.println(repository.findByFirstName("Jim"));
+		}
 
 }
