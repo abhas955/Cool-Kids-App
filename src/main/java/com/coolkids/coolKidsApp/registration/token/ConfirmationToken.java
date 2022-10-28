bcb2404630762e0d75cc07b706884aa3c05efbc4
Abdullahi <abhas955@gmail.com>
2022-10-27 21:49:21 -0400

    registration-and-login

---
+++ src/main/java/com/coolkids/coolKidsApp/registration/token/ConfirmationToken.java
@@ -0,0 +1,36 @@
+package com.coolkids.coolKidsApp.registration.token;
+
+import com.coolkids.coolKidsApp.model.User;
+import com.mongodb.lang.NonNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ConfirmationToken {
+    private Long id;
+    @NonNull
+    private String token;
+    @NonNull
+    private LocalDateTime createdAt;
+    @NonNull
+    private LocalDateTime expiredAt;
+    private LocalDateTime confirmedAt;
+
+    @DBRef()
+    private User user;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiredAt, LocalDateTime confirmedAt,User user) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+        this.confirmedAt = confirmedAt;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/coolkids/coolKidsApp/services/EmailValidator.java
@@ -0,0 +1,14 @@
+package com.coolkids.coolKidsApp.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+
+@Service
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+        //Todo Regex to validate email
+        return true;
+    }
+}
