6a5dc46ab67bd7342d08aca553caa98a982c52f8
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-01 10:53:03 -0400

    Created EventDTO.java class mirrored after UserDTO

---
+++ src/main/java/com/coolkids/coolKidsApp/api/v1/model/EventDTO.java
@@ -0,0 +1,27 @@
+package com.coolkids.coolKidsApp.api.v1.model;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+public class EventDTO {
+    private String id;
+    private LocalDateTime eventStartDateTime;
+    private LocalDateTime eventEndDateTime;
+    private LocalDate eventCreatedDate;
+    private LocalDate eventUpdatedDate;
+    private String eventType;
+    //private String eventPhoto;
+    private Integer maxAttendance;
+    private Integer currentRSVPS;
+    private String eventAddress;
+    private String eventDescription;
+    private String contactPersonName;
+    //private String contactPersonPicture;
+    private Integer contactPersonPhoneNumber;
+    private String contactPersonEmail;
+}
