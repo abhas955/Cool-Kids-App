fb08dadf540245538b05c63ae601f968c123f362
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-04 17:00:17 -0400

    Update EventServicesImpl file location
    Moved from api/services to coolKidsApp/services

---
+++ src/main/java/com/coolkids/coolKidsApp/services/EventServiceImpl.java
@@ -0,0 +1,28 @@
+package com.coolkids.coolKidsApp.services;
+
+import com.coolkids.coolKidsApp.api.v1.mapper.EventMapper;
+import com.coolkids.coolKidsApp.api.v1.model.EventDTO;
+import com.coolkids.coolKidsApp.repository.EventRepository;
+import com.coolkids.coolKidsApp.services.EventService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+
+    private final EventMapper eventMapper;
+
+    @Override
+    public List<EventDTO> getAllEvents() { //TODO: The heck?
+        return eventRepository.findAll() //TODO: The heck?
+                .stream() //TODO: The heck?
+                .map(eventMapper::eventToEventDTO) //TODO: The heck?
+                .collect(Collectors.toList()); //TODO: The heck?
+    }
+}
