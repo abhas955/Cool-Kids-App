cf7753e5b119be02f8d13363636196ba3271a1a5
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-04 16:55:37 -0400

    Created EventMapper
    Created class with EventMapper Instance and EventDTO

---
+++ src/main/java/com/coolkids/coolKidsApp/api/v1/mapper/EventMapper.java
@@ -0,0 +1,17 @@
+package com.coolkids.coolKidsApp.api.v1.mapper;
+
+import com.coolkids.coolKidsApp.api.v1.model.EventDTO;
+import com.coolkids.coolKidsApp.domain.Event;
+
+import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface EventMapper {
+
+    EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
+
+    @Mapping(source = "id",target = "id")
+    EventDTO eventToEventDTO(Event event);
+}
