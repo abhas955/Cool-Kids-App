0141a2dc564491c06d605b690f9568540178647a
Abdullahi <abhas955@gmail.com>
2022-11-04 19:59:45 -0400

    login test

--- src/main/java/com/coolkids/coolKidsApp/controllers/UserAccountController.java
+++ src/main/java/com/coolkids/coolKidsApp/controllers/UserAccountController.java
@@ -2,6 +2,7 @@ package com.coolkids.coolKidsApp.controllers;
 
 import com.coolkids.coolKidsApp.api.v1.model.UserDTO;
 import com.coolkids.coolKidsApp.api.v1.model.UserListDTO;
+import com.coolkids.coolKidsApp.domain.User;
 import com.coolkids.coolKidsApp.repository.UserRepository;
 
 import com.coolkids.coolKidsApp.services.RegistrationService;
@@ -9,7 +10,10 @@ import com.coolkids.coolKidsApp.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/")
@@ -24,13 +28,13 @@ public class UserAccountController {
         return registrationService.register(request);
     }
 
-//    @GetMapping("/login")
-//    public String listUsers(Model model){
-//        Optional<User> user = userRepository.findByEmail("email");
-//
-//        model.addAttribute("firstName", user);
-//        return "successfully logged in";
-//    }
+    @GetMapping("/login")
+    public String listUsers(Model model){
+        Optional<User> user = userRepository.findByEmail("email");
+
+        model.addAttribute("firstName", user);
+        return "successfully logged in";
+    }
 
     @GetMapping("/users")
     public ResponseEntity<UserListDTO> getAllUsers(){
