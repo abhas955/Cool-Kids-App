f67a021e5e9f05d53cdc3236ff8fb3738417e982
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-07 20:34:47 -0500

    Updated EventController
    Add several Todos. Also worked on createEvent method, but it isn't complete.

--- src/main/java/com/coolkids/coolKidsApp/controllers/EventController.java
+++ src/main/java/com/coolkids/coolKidsApp/controllers/EventController.java
@@ -1,5 +1,6 @@
 package com.coolkids.coolKidsApp.controllers;
 
+import com.coolkids.coolKidsApp.api.v1.model.EventDTO;
 import com.coolkids.coolKidsApp.api.v1.model.EventListDTO;
 
 import com.coolkids.coolKidsApp.services.EventService;
@@ -17,12 +18,25 @@ public class EventController {
     private final EventService eventService;
 
     //Todo: list all events
+    //TODO: Need to be logged in
     @GetMapping("/events")
     public ResponseEntity<EventListDTO> getAllEvents(){
         return new ResponseEntity<EventListDTO>(
                 new EventListDTO(eventService.getAllEvents()), HttpStatus.OK);
     }
     //Todo: create event
+    //Todo: Need to be logged in as an admin
+    //Todo: Create a new event document or part of document?
+    //Todo: Insert the document into the database
+    //Todo: Let User Know if successful or fail
+    //@PostMapping("/event/create")
+    //public String createEvent(@RequestBody CreateEvent )
+    //public ResponseEntity<EventDTO> createEvent(){
+        //return new ResponseEntity<EventDTO>(
+                //new EventDTO()(eventService.)
+        //)
+    //}
+
 
 
     //Todo: delete event
