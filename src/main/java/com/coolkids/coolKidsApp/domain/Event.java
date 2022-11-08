177e67991883d74ba79f7db0f6af558928819d5d
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-08 17:10:22 -0500

    Updated Event
    refactored event

--- src/main/java/com/coolkids/coolKidsApp/domain/Event.java
+++ src/main/java/com/coolkids/coolKidsApp/domain/Event.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
@@ -23,33 +24,19 @@ public class Event {
     @Id
     private String id;
     @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
-
     private LocalDateTime eventStartDateTime;
-
     private LocalDateTime eventEndDateTime;
-
     private LocalDate eventCreatedDate;
-
     private LocalDate eventUpdatedDate;
-
     private String eventType;
-
     //private String eventPhoto;
-
     private Integer maxAttendance;
-
     private Integer currentRSVPS;
-
     private String eventAddress;
-
     private String eventDescription;
-
     private String contactPersonName;
-
     //private String contactPersonPicture;
-
     private Integer contactPersonPhoneNumber;
-
     private String contactPersonEmail;
 
     public Event(String id, LocalDateTime eventStartDateTime, LocalDateTime eventEndDateTime,
