7877ed6b0b2df4e88d54d2b95ab4a42811b28cad
[Patrick Scott] <[pscott20@uncc.edu]>
2022-11-08 13:56:47 -0500

    Updated imageservice and imageserviceimpl
    Added saveimagefile method to both

--- src/main/java/com/coolkids/coolKidsApp/services/ImageService.java
+++ src/main/java/com/coolkids/coolKidsApp/services/ImageService.java
@@ -1,4 +1,8 @@
 package com.coolkids.coolKidsApp.services;
 
+import org.springframework.web.multipart.MultipartFile; //TODO: what is this for?
+
 public interface ImageService {
+
+    void saveImageFile(String eventId, MultipartFile file);
 }
