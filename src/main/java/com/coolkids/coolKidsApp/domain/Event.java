4b6f75d35f90a1eda03e12bf15e75c8825f61e44
[Patrick Scott] <89850257+pscott20@users.noreply.github.com>
2022-11-10 14:49:35 -0500

    The commented out stereotype in CreateEventRequest is for sending datetime in the future. Removed all of the unused setters in event.java.

--- src/main/java/com/coolkids/coolKidsApp/domain/Event.java
+++ src/main/java/com/coolkids/coolKidsApp/domain/Event.java
@@ -77,95 +77,47 @@ public class Event {
         return eventStartDateTime;
     }
 
-    public void setEventStartDateTime(LocalDateTime eventStartDateTime) {
-        this.eventStartDateTime = eventStartDateTime;
-    }
-
     public LocalDateTime getEventEndDateTime() {
         return eventEndDateTime;
-    }
-
-    public void setEventEndDateTime(LocalDateTime eventEndDateTime) {
-        this.eventEndDateTime = eventEndDateTime;
     }
 
     public LocalDate getEventCreatedDate() {
         return eventCreatedDate;
     }
 
-    public void setEventCreatedDate(LocalDate eventCreatedDate) {
-        this.eventCreatedDate = eventCreatedDate;
-    }
-
     public LocalDate getEventUpdatedDate() {
         return eventUpdatedDate;
-    }
-
-    public void setEventUpdatedDate(LocalDate eventUpdatedDate) {
-        this.eventUpdatedDate = eventUpdatedDate;
     }
 
     public String getEventType() {
         return eventType;
     }
 
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
     public Integer getMaxAttendance() {
         return maxAttendance;
-    }
-
-    public void setMaxAttendance(Integer maxAttendance) {
-        this.maxAttendance = maxAttendance;
     }
 
     public Integer getCurrentRSVPS() {
         return currentRSVPS;
     }
 
-    public void setCurrentRSVPS(Integer currentRSVPS) {
-        this.currentRSVPS = currentRSVPS;
-    }
-
     public String getEventAddress() {
         return eventAddress;
-    }
-
-    public void setEventAddress(String eventAddress) {
-        this.eventAddress = eventAddress;
     }
 
     public String getEventDescription() {
         return eventDescription;
     }
 
-    public void setEventDescription(String eventDescription) {
-        this.eventDescription = eventDescription;
-    }
-
     public String getContactPersonName() {
         return contactPersonName;
-    }
-
-    public void setContactPersonName(String contactPersonName) {
-        this.contactPersonName = contactPersonName;
     }
 
     public Integer getContactPersonPhoneNumber() {
         return contactPersonPhoneNumber;
     }
 
-    public void setContactPersonPhoneNumber(Integer contactPersonPhoneNumber) {
-        this.contactPersonPhoneNumber = contactPersonPhoneNumber;
-    }
-
     public String getContactPersonEmail() {
         return contactPersonEmail;
-    }
-
-    public void setContactPersonEmail(String contactPersonEmail) {
-        this.contactPersonEmail = contactPersonEmail;
     }
 }
