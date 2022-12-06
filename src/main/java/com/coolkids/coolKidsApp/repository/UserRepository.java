0c49820ab1c6ca7043f102dc09911e5b49a9a3cd
Abdullahi <abhas955@gmail.com>
2022-12-06 02:41:02 -0500

    rsvp endpoint fixed

--- src/main/java/com/coolkids/coolKidsApp/repository/UserRepository.java
+++ src/main/java/com/coolkids/coolKidsApp/repository/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Repository
 //@Transactional(readOnly = true)
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
